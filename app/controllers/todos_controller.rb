aee946ca1fe70ef518a31d3229355dd3dcd7f454
daniel.hahn <daniel.hahn@G215-test.ig-edu.univ-paris13.fr.ig-edu.univ-paris13.fr>
2012-02-22 17:00:05 +0100

    Added a button to update the done status on the items

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
 
   def index
-    @todos = Todo.all
+    @todos = Todo.not_done
   end
 
   def new
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,3 @@
 class Todo < ActiveRecord::Base
+  scope :not_done, :conditions => "done IS NULL OR done is 0"
 end
