c19368799fabc0c79b4d289383f3cb20286589da
daniel.hahn <daniel.hahn@G215-test.ig-edu.univ-paris13.fr.ig-edu.univ-paris13.fr>
2012-02-22 16:43:36 +0100

    Updating done status

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,4 +14,10 @@ class TodosController < ApplicationController
     redirect_to todos_url
   end
 
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(params[:todo])
+    redirect_to todos_url
+  end
+
 end
