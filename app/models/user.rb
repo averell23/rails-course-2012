19e5a4a07659db6d696ecb06271ebc2ecaff7aa4
daniel.hahn <daniel.hahn@G215-test.ig-edu.univ-paris13.fr.ig-edu.univ-paris13.fr>
2012-02-23 11:15:13 +0100

    New user model and controller

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120223100854) do
+
+  create_table "todos", :force => true do |t|
+    t.string   "description"
+    t.boolean  "done"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
