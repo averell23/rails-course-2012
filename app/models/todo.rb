e74ff4b8ca7d188283a94d312a932062ccec4346
Daniel Hahn <ghub@limitedcreativity.org>
2012-02-23 13:07:49 +0100

    Fix little bug in scope

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ActiveRecord::Base
-  scope :not_done, :conditions => "done IS NULL OR done = 0"
+  scope :not_done, :conditions => "done IS NULL OR done = '0'"
 
   validates_length_of :description, :minimum => 4
 end
