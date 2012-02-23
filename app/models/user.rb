9a1560270121cd4040429c9e3db468f02d5239f4
daniel.hahn <daniel.hahn@G215-test.ig-edu.univ-paris13.fr.ig-edu.univ-paris13.fr>
2012-02-23 14:11:34 +0100

    Added relation between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :todos
+
   validates_presence_of :name, :email
   validates_uniqueness_of :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120223100854) do
+ActiveRecord::Schema.define(:version => 20120223125153) do
 
   create_table "todos", :force => true do |t|
     t.string   "description"
     t.boolean  "done"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
