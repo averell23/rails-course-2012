36c05438a8676439eb41e59bce22c1f7750e6750
daniel.hahn <daniel.hahn@G215-test.ig-edu.univ-paris13.fr.ig-edu.univ-paris13.fr>
2012-02-23 11:33:46 +0100

    More users stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+ def index
+   @users = User.all
+ end
+
+ def new
+   @user = User.new
+ end
+
+ def create
+   @user = User.new(params[:user])
+   if(@user.save)
+     redirect_to users_path
+   else
+     render :new
+   end
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ MyNewAppByHand::Application.routes.draw do
   # first created -> highest priority.
 
   resources :todos
+  resources :users
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
